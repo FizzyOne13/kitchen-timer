5382cdee8d8dd165ad0500d81c54de7aff6868af
FizzyOne13 <119670800+FizzyOne13@users.noreply.github.com>
2023-01-25 23:56:20 +0200

    That's not working anyway
    Kinda dumb backup, but never mind

---
+++ scripts/scripts.js
@@ -0,0 +1,15 @@
+function openLog() {
+  document.getElementById("logForm").style.display = "block";
+}
+
+function openReg() {
+  document.getElementById("regForm").style.display = "none";
+}
+
+function closeLog() {
+  document.getElementById("logForm").style.display = "block";
+}
+
+function closeReg() {
+  document.getElementById("regForm").style.display = "none";
+}
