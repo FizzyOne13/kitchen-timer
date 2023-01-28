ecaadae735e08b15d0478c4cc20cb633cfdb6096
FizzyOne13 <119670800+FizzyOne13@users.noreply.github.com>
2023-01-28 21:01:45 +0200

    Intermediate with half-func up-right LRbox
    Main issue for now - to force "show more" button work as intended. cross button +/- works. form in general - not
    backing-up just in case

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,3 +13,22 @@ function closeLog() {
 function closeReg() {
   document.getElementById("regForm").style.display = "none";
 }
+
+function closeLRblock() {
+  document.getElementById("LRblock").style.display = "none";
+  showShow();
+}
+
+function showShow() {
+  document.getElementById("LRopen").style.display = "block";
+}
+
+function openLRblock() {
+  document.getElementById("LRbblock").style.display = "block";
+}
+
+const greenCross = document.getElementById("LRblock");
+
+greenCross.addEventListener("click", function handleClick(event) {
+  event.target.remove();
+});
