9826122cf8f95973d71521cb0c58ff99f840659c
FizzyOne13 <119670800+FizzyOne13@users.noreply.github.com>
2023-03-21 16:42:09 +0200

    Before timers - finish line pre-beta
    Of major just actual timer and their functional left - the other thing like some basic back-end, more settings option expected to be far later, after i step aside and do some other projects.
    
    Fixed: timer charcount, now independent
    impossible transparency setting without color switch
    settings popup menu
    autopref & post-css
    
    ToDo: timers
    sort out devDeps and modules if non-used
    
    Perspective: more settings and back-end basics

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -77,20 +77,11 @@ function closeSettings() {
   document.getElementById("settingsForm").style.display = "none";
 }
 //page sizing and footer placement depending on quantaty of timers and device
-const body = document.getElementById("body");
-const header = document.getElementById("head");
-const foot = document.getElementById("foot");
 const root = document.documentElement;
 window.onload = function getCentrHeight() {
   const centrCont = document.querySelector(".central-container");
   const currentCentrHight = centrCont.offsetHeight;
-  body.style.setProperty(
-    "height",
-    "calc(currentCentrHight + (--centralContrTopMarg)  + (--minBotMarg))"
-  );
-  root.style.setProperty("--footerTopMarg", currentCentrHight + 450 + "px");
-  foot.style.marginTop =
-    "calc(currentCentrHight + (--centralContrTopMarg) + (--minBotMarg))";
+  root.style.setProperty("--footerTopMarg", currentCentrHight + 430 + "px");
 };
 
 //color schemes switching
@@ -147,12 +138,16 @@ function scheme() {
       store.style.setProperty("--bold-color", "rgb(154, 27, 2)");
       store.style.setProperty("--lighter-color", "rgb(239, 166, 141)");
       store.style.setProperty("--background-color", "rgba(216, 124, 91, 0.2)");
+    } else {
+      store.style.setProperty("--bold-color", "rgb(154, 27, 2)");
+      store.style.setProperty("--lighter-color", "rgb(239, 166, 141)");
+      store.style.setProperty("--background-color", "rgba(216, 124, 91, 0.2)");
     }
   } else {
     if (document.getElementById("set-blue").checked == true) {
       store.style.setProperty("--bold-color", "rgb(7, 94, 114)");
       store.style.setProperty("--lighter-color", "rgb(122, 206, 242)");
-      store.style.setProperty("--background-color", "rgba(99, 148, 212)");
+      store.style.setProperty("--background-color", "rgb(99, 148, 212)");
     } else if (document.getElementById("set-green").checked == true) {
       store.style.setProperty("--bold-color", "rgb(7, 114, 59)");
       store.style.setProperty("--lighter-color", "rgb(97, 245, 225)");
@@ -162,6 +157,10 @@ function scheme() {
       store.style.setProperty("--lighter-color", "rgb(239, 232, 132)");
       store.style.setProperty("--background-color", "rgb(204, 216, 91)");
     } else if (document.getElementById("set-red").checked == true) {
+      store.style.setProperty("--bold-color", "rgb(154, 27, 2)");
+      store.style.setProperty("--lighter-color", "rgb(239, 166, 141)");
+      store.style.setProperty("--background-color", "rgb(216, 124, 91)");
+    } else {
       store.style.setProperty("--bold-color", "rgb(154, 27, 2)");
       store.style.setProperty("--lighter-color", "rgb(239, 166, 141)");
       store.style.setProperty("--background-color", "rgb(216, 124, 91)");
@@ -208,17 +207,11 @@ function addTimer() {
   timerDiv.id = "timer" + n;
   timerText.id = "area" + n;
   centrGrid.insertBefore(timerDiv, centrGrid.children[n - 1]);
-  var lastTimer = document.getElementById("timer" + n);
-  //var lastArea = document.getElementById("area" + n);
-  //lastTimer.querySelector(".title-bar").value = "";
-  lastTimer
-    .querySelector(".title-bar")
-    .setAttribute("placeholder", "Your title");
-  // lastTimer.querySelector(".timer-bar").value = "";
-  // lastTimer.querySelector(".timer-description").value = "";
-  lastTimer
-    .querySelector(".timer-description")
-    .setAttribute("placeholder", "Here you can leave a short hint");
+  const timers = centrGrid.children;
+  for (i = timers.length - 2; i < timers.length - 1; i++) {
+    centrGrid.children[i].querySelector(".title-bar").value = "";
+    centrGrid.children[i].querySelector(".timer-description").value = "";
+  }
   letsGo();
 }
 
@@ -232,26 +225,10 @@ function delTimer(btn) {
   idSet();
 }
 
-//let getValue = () => qq.getAttribute("value");
-var y = document.getElementById("time1");
-function setTime() {
-  //const t = qq.getAttribute("input").value;
-  //console.log(t);
-  //var y = document.getElementById("time1");
-  //var yy = y.querySelector(".timer");
-  var a = y.getComputedStyle("::-webkit-datatime-edit");
-  //var qqq = q.getElementsByClassName("-webkit-datetime-edit-hour-field");
-  // var g = qqq.getAttribute("aria-valuetext");
-  console.log(a);
-}
-
 //remaining characters in text-areas
-let textAreas = document.querySelectorAll(".timer-description");
 const CharCount = document.querySelector(".timer-description-chars-left");
 let clickEvent = (item) => {
   const target = item.nextElementSibling;
-  //console.log(item);
-  //console.log(target);
   const MaxChars = item.getAttribute("maxlength");
   const CurChars = item.value.length;
   const CharsLeft = MaxChars - CurChars;
@@ -260,7 +237,22 @@ let clickEvent = (item) => {
   target.style.color = colorRed;
 };
 function letsGo() {
+  const textAreas = document.querySelectorAll(".timer-description");
   textAreas.forEach((item) => {
     item.addEventListener("input", clickEvent(item));
   });
+}
+
+//setting backgorund pattern
+const setPatt = document.getElementById("patt");
+function setPat() {
+  if (setPatt.checked) {
+    document.body.style.backgroundImage = "";
+    document.body.style.backgroundRepeat = "";
+    document.body.style.backgroundSize = "";
+  } else {
+    document.body.style.backgroundImage = "url(/images/pattern.png)";
+    document.body.style.backgroundRepeat = "repeat";
+    document.body.style.backgroundSize = "contain";
+  }
 }
